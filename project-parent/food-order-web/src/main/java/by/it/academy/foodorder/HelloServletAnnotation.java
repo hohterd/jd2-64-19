ce4e0b45069606b085d1350f987147cec2f97ea9
hohterd <you@example.com>
2019-12-04 22:01:06 +0300

    added training servlets

---
+++ project-parent/food-order-web/src/main/java/by/it/academy/foodorder/HelloServletAnnotation.java
@@ -0,0 +1,19 @@
+package by.it.academy.foodorder;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloServletAnnotation", urlPatterns = "/sayhelloannotation")
+public class HelloServletAnnotation extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("<h2>Common HelloServlet with annotation</h2>");
+    }
+}
---
+++ project-parent/food-order-web/src/main/java/by/it/academy/foodorder/HelloServletCommon.java
@@ -0,0 +1,17 @@
+package by.it.academy.foodorder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServletCommon extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("<h2>Common HelloServlet added in web.xml</h2>");
+    }
+}
