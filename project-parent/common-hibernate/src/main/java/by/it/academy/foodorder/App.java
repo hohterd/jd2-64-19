c5798025501c9db4598b0224653f68ea9a6fb13b
hohterd <you@example.com>
2020-01-26 23:18:17 +0300

    Orm relations

--- project-parent/common-hibernate/src/main/java/by/it/academy/foodorder/App.java
+++ project-parent/common-hibernate/src/main/java/by/it/academy/foodorder/App.java
@@ -1,9 +1,15 @@
 package by.it.academy.foodorder;
 
-import by.it.academy.foodorder.entity.Address;
-import by.it.academy.foodorder.entity.Person;
+import by.it.academy.foodorder.hierarchy.Departament;
+import by.it.academy.foodorder.hierarchy.Employee;
+import by.it.academy.foodorder.hierarchy.EmployeeDetail;
+import by.it.academy.foodorder.hierarchy.Meeting;
 import by.it.academy.foodorder.util.HibernateUtil;
 import org.hibernate.Session;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 
 /**
@@ -14,18 +20,41 @@ public class App
 {
     public static void main( String[] args ){
         Session session = HibernateUtil.getSessionFactory().openSession();
+        /*//OneToOne
         session.beginTransaction();
+        employee = new Employee(null, "Sergey",
+                "Korolyov", LocalDateTime.now(), null, null);
+        EmployeeDetail detail = new EmployeeDetail(null, "Street", "City",
+                "State", "Country", null);
+        employee.setEmployeeDetail(detail);
+        detail.setEmployee(employee);
+        session.save(detail);
+        session.getTransaction().commit();
+        session.close();*/
 
-        Person person = new Person(null, "serg","koro", 23, new Address("Kalvariyskaya", "Minsk", "222000"), null);
+        /*//OneToMany
+        session.beginTransaction();
+        Departament departament = new Departament("Dev");
+        employee = new Employee(null, "Sergey",
+                "Korolyov", LocalDateTime.now(), null, departament);
+        session.save(departament);
+        session.save(employee);
+        session.getTransaction().commit();
+        session.close();*/
 
-        session.save(person);
+
+        //ManyToMany
+        session.beginTransaction();
+        Employee employee = new Employee(null, "Sergey", "Korolyov",
+                LocalDateTime.now(), null, null, new ArrayList<>());
+        Meeting meeting = new Meeting("ManyToMany");
+        meeting.getEmployees().add(employee);
+        session.save(meeting);
+        employee.getMeetings().add(meeting);
+        session.saveOrUpdate(employee);
         session.getTransaction().commit();
         session.close();
 
-        session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-        System.err.println(session.get(Person.class, 1L));
-        session.close();
         HibernateUtil.shutdown();
     }
 }
